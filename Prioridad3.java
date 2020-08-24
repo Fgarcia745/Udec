c4acef0bf43017a5360ec9b132ada00a235aec7f
Fgarcia745 <64146304+Fgarcia745@users.noreply.github.com>
2020-08-24 13:04:21 -0500

    Delete Prioridad3.java

--- Prioridad3.java
+++
@@ -1,7 +0,0 @@
-public class Prioridad3 {//clase principal 
-    public void prioridad3() {//metodo sin retorno ni parametros 
-      int y=21;//declacla un avariable entera 'y' asignada con '21'
-      int x = y*(3+5) ;//declara una variable entera 'x' asignada con 'y' y operada con enteros
-      System.out.println(x);//imprime 'x'
-    }
-}
