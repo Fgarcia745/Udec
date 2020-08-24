697848709297d8ae69a3fb124f6af43d07f3e136
Fgarcia745 <64146304+Fgarcia745@users.noreply.github.com>
2020-08-24 12:55:28 -0500

    Delete If.java

--- If.java
+++
@@ -1,7 +0,0 @@
-public class If {
-    public void iF() {
-        int puntuacion=9;//declara una variable de tipo entero 'int' asignada con 9
-        //valida la variable 'puntuacion' y la hace diferente de 5 e imprime un mensaje en caso cumplir esta validacion
-     if (puntuacion != 5) System.out.println("¡Tienes un aprobado raspado!" ) ;
-    }
-}
